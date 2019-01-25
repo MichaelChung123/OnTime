5b3f2292e522f5d5fac2cd5c127c57cb20796205
Daniel KwanUk Yoo <kwanukyoo@gmail.com>
2019-01-24 18:13:27 -0800

    test

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
     render(){
         return(
-            <h1>Hello World this is the app.js testing</h1>
+            <h1>Hello World this is the app.js testing...</h1>
         )
     }
 }
