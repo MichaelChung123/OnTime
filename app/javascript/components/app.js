2bd337053a7ce8b4561c4f5ccb2abf72a3f48444
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 10:29:24 -0800

    finally got navbar to display at same time as sidebar displays - need to style

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import NavBar from './nav/navbar'
 import SideBar from './sides/sidebar'
 import SideEmployee from './sides/sideEmployee'
 
 export default class App extends React.Component {
     render(){
         return(
+          <div>
+            <NavBar />
             <SideBar />
+          </div>
         )
     }
 }
---
+++ app/javascript/components/nav/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const NavBar = props => (
+  <header className="navbar">
+    <nav className="toolbar_navigation">
+      <div className="navbar_logo"><a href="/">THE LOGO</a></div>
+      <div className="spacer" />
+      <div className="navbar_navigation-items">
+        <ul>
+          <li><a href="/">Products</a></li>
+          <li><a href="/">Users</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
+
+export default NavBar
