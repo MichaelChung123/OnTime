c964df29c5b906b33ec0b08dd68730861b5fc145
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 10:43:06 -0800

    moved styling to welcome.scss file. deleted individual navbar.css file

--- app/javascript/components/nav/navbar.js
+++ app/javascript/components/nav/navbar.js
@@ -5,12 +5,12 @@ import React from 'react';
 const NavBar = props => (
   <header className="navbar">
     <nav className="navbar_navigation">
-      <div className="navbar_logo"><a href="/">THE LOGO</a></div>
+      <div className="navbar_logo"><a href="/">STARBOARD</a></div>
       <div className="spacer" />
       <div className="navbar_navigation-items">
         <ul>
-          <li><a href="/">Products</a></li>
-          <li><a href="/">Users</a></li>
+          <li><a href="/">Schedule</a></li>
+          <li><a href="/">Profile</a></li>
         </ul>
       </div>
     </nav>
