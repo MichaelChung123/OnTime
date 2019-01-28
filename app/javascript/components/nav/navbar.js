b51135c305f9bfae2bf02d68837a8e4e64d26e04
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 10:37:06 -0800

    moved sidebar down so that nav displays across whole top

--- app/javascript/components/nav/navbar.js
+++ app/javascript/components/nav/navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const NavBar = props => (
   <header className="navbar">
-    <nav className="toolbar_navigation">
+    <nav className="navbar_navigation">
       <div className="navbar_logo"><a href="/">THE LOGO</a></div>
       <div className="spacer" />
       <div className="navbar_navigation-items">
