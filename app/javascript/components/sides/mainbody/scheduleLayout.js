ccf1cff66772b7ed5cdd7450fd5b57a59ed75136
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-01-28 14:09:15 -0800

    commit before pull

--- app/javascript/components/sides/mainbody/scheduleLayout.js
+++ app/javascript/components/sides/mainbody/scheduleLayout.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class ScheduleLayout extends React.Component {
     render() {
         return(
-            <h1>this is going to schedule layout</h1>
+            <h1>hiiiii</h1>
         )
     }
 }
