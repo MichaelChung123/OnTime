3033e8bc2b51068226dfda7db0dc8d71d0d3ee19
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 18:37:43 -0800

    adding flexbox to footer container

--- app/javascript/components/footer/footer.js
+++ app/javascript/components/footer/footer.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const Footer = props => (
-  <footer className="footer">
-    <nav className="footer_navigation">
-      <div className="footer_logo"><a href="/">FOOT</a></div>
-      <div className="spacer" />
+  <footer className="logistics_footer">
       <div className="footer_navigation-items">
         <ul>
-          <li><a href="/">Foot</a></li>
-          <li><a href="/">Fetish</a></li>
+          <li><a href="/">Number of Students: </a></li>
+          <spacer />
+          <li><a href="/">Number of Mentors Needed: </a></li>
         </ul>
       </div>
-    </nav>
+
+
   </footer>
 )
 
