729f2d7a4f2a43c088c7588fa86434bad4a0f9bb
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 19:51:21 -0800

    imported api for styling footer logistics

--- app/javascript/components/footer/footer.js
+++ app/javascript/components/footer/footer.js
@@ -1,12 +1,16 @@
+
+
+
 import React from 'react';
 
 const Footer = props => (
   <footer className="logistics_footer">
-      <div className="footer_navigation-items">
-        <ul>
-          <li><a href="/">Number of Students: </a></li>
-          <li><a href="/">Number of Mentors Needed: </a></li>
-        </ul>
+      <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell"></div>
+        <div class="mdc-layout-grid__cell"></div>
+        <div class="mdc-layout-grid__cell"></div>
+      </div>
       </div>
 
 
