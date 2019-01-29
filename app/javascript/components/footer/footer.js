6030c7752ea2b936d4cd80aac33dca65dd5e61d1
Michael Chung <michael.chung1994@gmail.com>
2019-01-29 13:05:59 -0800

    pulled ally's push to master, confirmed working.

--- app/javascript/components/footer/footer.js
+++ app/javascript/components/footer/footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Footer = props => (
-  <footer class="logistics_footer">
-      <div class="flex-container">
+  <footer className="logistics_footer">
+      <div className="flex-container">
 
-        <div class="flex-child">
+        <div className="flex-child">
           <h4>Number of Students</h4><input number="Number of students" type="text"></input>
 
         </div>
 
-        <div class="flex-child"></div>
-        <div class="flex-child"></div>
+        <div className="flex-child"></div>
+        <div className="flex-child"></div>
 
       </div>
 
