2fa05b872ab3b6e290e0147719cbb69ab8c9fc1b
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-01-28 16:49:10 -0800

    footer appears - need to fill

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './nav/navbar'
 import SideBar from './sides/sidebar'
+import Footer from './footer/footer'
 import SideEmployee from './sides/sideEmployee'
 import ScheduleLayout from './sides/mainbody/scheduleLayout';
 
@@ -11,6 +12,7 @@ export default class App extends React.Component {
                 <NavBar />
                 <ScheduleLayout />,
                 <SideBar />
+                <Footer />
             </div>
         )
     }
---
+++ app/javascript/components/footer/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = props => (
+  <footer className="footer">
+    <nav className="footer_navigation">
+      <div className="footer_logo"><a href="/">FOOT</a></div>
+      <div className="spacer" />
+      <div className="footer_navigation-items">
+        <ul>
+          <li><a href="/">Foot</a></li>
+          <li><a href="/">Fetish</a></li>
+        </ul>
+      </div>
+    </nav>
+  </footer>
+)
+
+export default Footer
