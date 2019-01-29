8e5e6492659307d4b22c627fd7631bb4d7832725
Michael Chung <michael.chung1994@gmail.com>
2019-01-29 13:00:41 -0800

    re-arranged code to make navbar transition between pages. also added edit employee bar

--- app/javascript/components/sides/sideEmployee.js
+++ app/javascript/components/sides/sideEmployee.js
@@ -7,7 +7,8 @@ export default class SideEmployee extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            editSelected: false
+            editSelected: false,
+            expanded: true
         }
         this.editEmployee = this.editEmployee.bind(this);
     }
@@ -35,60 +36,72 @@ export default class SideEmployee extends React.Component {
         });
 
         return (
-            <SideNav>
+            <div>
                 {!this.state.editSelected &&
-                    <SideNav.Nav>
-                        <NavItem eventKey="add-shift">
-                            <NavIcon>
-                                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                            </NavIcon>
-                            <NavText onClick={() => this.props.back()}>
-                                Back
+                    <SideNav expanded={this.state.expanded}
+                        onToggle={(expanded) => {
+                            this.setState({ expanded: !this.state.expanded });
+                        }}
+                    >
+                        <SideNav.Toggle />
+                        <SideNav.Nav>
+                            <NavItem eventKey="add-shift">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText onClick={() => this.props.back()}>
+                                    Back
                             </NavText>
-                        </NavItem>
+                            </NavItem>
 
-                        <div className="container">
-                            <div className="row profile">
-                                <div className="col-md-3">
-                                    <div className="profile-sidebar">
+                            <div className="container">
+                                <div className="row profile">
+                                    <div className="col-md-3">
+                                        <div className="profile-sidebar">
 
-                                        <div className="profile-userpic">
-                                            {/* <img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" className="img-responsive" alt=""> */}
-                                        </div>
-
-                                        <div className="profile-usertitle">
-                                            <div className="profile-usertitle-name">
-                                                {this.props.employee.first_name} {this.props.employee.last_name}
+                                            <div className="profile-userpic">
+                                                {/* <img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" className="img-responsive" alt=""> */}
                                             </div>
-                                            <div className="profile-usertitle-job">
-                                                {this.props.employee.occupation}
+
+                                            <div className="profile-usertitle">
+                                                <div className="profile-usertitle-name">
+                                                    {this.props.employee.first_name} {this.props.employee.last_name}
+                                                </div>
+                                                <div className="profile-usertitle-job">
+                                                    {this.props.employee.occupation}
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div className="profile-userbuttons">
-                                            <button type="button" className="btn btn-success btn-sm" onClick={this.editEmployee}>Edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm">Contact</button>
-                                        </div>
+                                            <div className="profile-userbuttons">
+                                                <button type="button" className="btn btn-success btn-sm" onClick={this.editEmployee}>Edit</button>
+                                                <button type="button" className="btn btn-danger btn-sm">Contact</button>
+                                            </div>
 
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <NavItem>
-                            <NavText>
-                                Scheduled Shifts
-                    </NavText>
-                        </NavItem>
 
-                        {shifts}
+                            <NavItem>
+                                <NavIcon>
 
-                    </SideNav.Nav>
+                                </NavIcon>
+                                <NavText>
+                                    Scheduled Shifts
+                                </NavText>
+                            </NavItem>
+
+                            {shifts}
+
+                        </SideNav.Nav>
+                    </SideNav>
+
                 }
                 {this.state.editSelected &&
                     <EditEmployee back={this.props.back} employee={this.props.employee} />
                 }
-            </SideNav>
+            </div>
         )
     }
 }
--- app/javascript/components/sides/sidebar.js
+++ app/javascript/components/sides/sidebar.js
@@ -13,7 +13,8 @@ export default class SideBar extends React.Component {
             employees: [],
             shifts: [],
             showInfo: false,
-            clicked: false
+            clicked: false,
+            expanded: true
         };
 
 
@@ -70,63 +71,65 @@ export default class SideBar extends React.Component {
         const shifts = this.state.shifts;
 
         return (
-            <SideNav
-                onSelect={(selected) => {
-
-                }}
-            >
-                <SideNav.Toggle />
+            <div>
                 {!this.state.showInfo &&
-                    <SideNav.Nav>
-                        <NavItem eventKey="add-shift" onClick={() => this.addShiftHandleClick()}>
-                            <NavIcon>
-                                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                            </NavIcon>
-                            <NavText>
-                                + Add Shift
-                        </NavText>
-                        </NavItem>
-
-                        <ReactCSSTransitionGroup
-                            transitionName="popup_css"
-                            transitionEnterTimeout={500}
-                            transitionLeaveTimeout={300}
-                        >
-                            {this.state.clicked ? <Popup closePopup={this.addShiftHandleClick} listOfEmployees={this.state.employees} /> : null}
-                        </ReactCSSTransitionGroup>
-
-                        <NavItem eventKey="add-employee">
-                            <NavIcon>
-                                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-                            </NavIcon>
-                            <NavText>
-                                + Add Employee
-                        </NavText>
-                            <NavItem eventKey="employee/subitem1">
+                    <SideNav expanded={this.state.expanded}
+                        onToggle={(expanded) => {
+                            this.setState({ expanded: !this.state.expanded });
+                        }}
+                    >                        
+                    <SideNav.Toggle />
+                        <SideNav.Nav>
+                            <NavItem eventKey="add-shift" onClick={() => this.addShiftHandleClick()}>
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
                                 <NavText>
-                                    Test Sub Item 1
+                                    + Add Shift
+                        </NavText>
+                            </NavItem>
+
+                            <ReactCSSTransitionGroup
+                                transitionName="popup_css"
+                                transitionEnterTimeout={500}
+                                transitionLeaveTimeout={300}
+                            >
+                                {this.state.clicked ? <Popup closePopup={this.addShiftHandleClick} listOfEmployees={this.state.employees} /> : null}
+                            </ReactCSSTransitionGroup>
+
+                            <NavItem eventKey="add-employee">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    + Add Employee
+                        </NavText>
+                                <NavItem eventKey="employee/subitem1">
+                                    <NavText>
+                                        Test Sub Item 1
+                            </NavText>
+                                </NavItem>
+                                <NavItem eventKey="employee/subitem2">
+                                    <NavText>
+                                        Test Sub Item 2
+                            </NavText>
+                                </NavItem>
+                            </NavItem>
+                            <NavItem>
+                                <NavText>
+                                    Employees
                             </NavText>
                             </NavItem>
-                            <NavItem eventKey="employee/subitem2">
-                                <NavText>
-                                    Test Sub Item 2
-                            </NavText>
-                            </NavItem>
-                        </NavItem>
-                        <NavItem>
-                            <NavText>
-                                Employees
-                            </NavText>
-                        </NavItem>
-                        {employees}
+                            {employees}
 
-                    </SideNav.Nav>
+                        </SideNav.Nav>
+                    </SideNav>
+
                 }
                 {this.state.showInfo &&
                     <SideEmployee shifts={shifts} employee={this.state.employee} back={this.back} />
                 }
-            </SideNav>
-
+            </div>
         )
     }
 }
