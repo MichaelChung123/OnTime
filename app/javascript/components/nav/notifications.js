65a80ea11e47f2b95bea6278db7f5f217055cd61
Michael Chung <michael.chung1994@gmail.com>
2019-02-03 18:01:51 -0800

    nothing changed

---
+++ app/javascript/components/nav/notifications.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Notifications extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            expanded: true
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
