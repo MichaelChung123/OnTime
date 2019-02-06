fd6b61f0e3026925312de04dc43fbf055392af94
Allison Avis <allisonavis@Allisons-MacBook-Pro.local>
2019-02-06 00:56:02 -0800

    checking everything works

--- app/javascript/components/EmployeeApp/empScheduleApp.js
+++ app/javascript/components/EmployeeApp/empScheduleApp.js
@@ -19,7 +19,7 @@ export default class ScheduleApp extends React.Component {
 
     render() {
         return(
-            <div>
+            <div className="employee-schedule-container">
                 <h2 className="employee-calendar-title">Welcome! Don Burks</h2>
                 <Calendar employees={this.state.employees} shifts={this.state.shifts} fetchDate={this.props.fetchDate} />
             </div>
