221585ee543659e10577a7a9d83ef0d77f8e79b4
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-06 00:42:30 -0800

    changed name mentorcalc to logistsics

--- app/javascript/components/logistics/logistics.js
+++ app/javascript/components/logistics/logistics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class MentorCalculator extends React.Component {
+export default class Logistics extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
