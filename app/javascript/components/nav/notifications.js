a2bc9c6f7549ad916a96848486e20f70c54004ad
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-02-05 22:19:10 -0800

    working on notifications

--- app/javascript/components/nav/notifications.js
+++ app/javascript/components/nav/notifications.js
@@ -9,23 +9,30 @@ export default class Notifications extends React.Component {
         }
     }
 
+    acceptRequest = () => {
+        console.log(`accp`)
+    }
+
+    declineRequest = () => {
+        console.log(`dec`)
+    }
+
     render() {
 
         let notifications = this.props.notifications.map((e, index) => {
             return (
-                [<label key={index + 1}>{e}</label>, <br key={-index - 1} />]
+                <li key={index} data-key={index + 1}>{e}<br/><button onClick={() => {this.acceptRequest()}}>Accept</button><button onClick={() => this.declineRequest()}>Decline</button><br/></li>
             );
         });
-
+        
         return (
             <div className="popup">
                 <div className="popup-form">
                     <label htmlFor="employee">Requests</label>
                     <div className="styled-select">
                         {notifications}
-                        <br />
-                        <br />
-                        <br />
+                    <br/>
+                    <br/>
                     </div>
                     <button className="form_button close" onClick={() => this.props.back()}>Close</button>
                 </div>
